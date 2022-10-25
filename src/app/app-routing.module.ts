3c96dc31af74e2d92f42da7d46661199ad05482d
ANU J PILLAI <107497512+BEAST-0@users.noreply.github.com>
2022-10-25 12:17:56 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [{ path: '', component: PortfolioComponent }];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [AppComponent, PortfolioComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
